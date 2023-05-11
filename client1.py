fe13c75088342aadf5d560964135a6006fc46cfa
malkeetsingh01 <malkeet0166@gmail.com>
2023-05-11 16:57:57 +0530

    formatted code

--- client1.py
+++ client1.py
@@ -1,104 +1,55 @@
-
-
 import threading
-
 import requests
-
 import json
-
 import time
-
-
 
 URL='http://127.0.0.1:8000'
 
 
 cached_error=""
 
-
-
+global user 
+global friend
 
 # URL='127.0.0.1:8000'
 
-
-
 def get_message(user,friend):
-
     param={
-
         'param1':user,
-
         'param2':friend
-
     }
-
     while True:
-
         res=requests.get(URL+"/message",params=param)
-
         # print(res.status_code)
-
         time.sleep(1)
-
         # if res.text :print(res.text)
-
         if res.text!="" and cached_error!=res.text:
-
-          print("[{}]-{}".format(friend,res.text))
-
+          print("\n[{}]-{}".format(friend,res.text))
           cached_error=res.text
-
         else: cached_error=res.text
-
         # print(res.text)
 
-
-
 def post_message(user,friend,message):
-
     payload={
-
         "user":user,
-
         "friend":friend,
-
         "message":message
-
     }
-
     # print(payload)
-
     res=requests.post(URL,data=json.dumps(payload))
-
     return res.status_code
 
-
-
 def chat():
-
     user=input("Enter user: ")
-
     friend=input("Enter friend: ")
-
     t=threading.Thread(target=get_message,args=(user,friend))
-
     t.start()
-
     res=200
-
     while True and res==200:
-
         time.sleep(1)
-
-        # message=input("[{}]-".format(user))
-
-        message=input()
+        message=input("[{}]-".format(user))
         res=post_message(user,friend,message)
-
     t.join()
 
-
-
 if __name__ == '__main__':
-
     chat()
--- server1.py
+++ server1.py
@@ -1,126 +1,63 @@
-
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
 import time
-
 from urllib.parse import urlparse, parse_qs
-
-
 
 import json
 
-
-
 HOST="127.0.0.1"
-
-
 
 PORT=8000
 
-
-
 messageList={}
-
-
 
 # cached_message
 
-
-
 class CustomConnection(BaseHTTPRequestHandler):
-
     def do_GET(self):
-
         query = urlparse(self.path).query
-
         data= parse_qs(query)
-
         print(data)
-
         # print(self.path)
-
         path = urlparse(self.path).path
-
         print(path)
-
         if(path=="/message"):
-
             self.send_response(200)
-
             self.send_header("Content-Type","text/html")
-
             self.end_headers()
-
             key_hash=str(data['param2'][0].lower()+"%"+data['param1'][0].lower())
-
             # type(key_hash)
-
             # print(str(messageList[key_hash][-1]))
-
             if key_hash in messageList and messageList[key_hash]:
-
                 self.wfile.write(bytes(str(messageList.get(key_hash)[-1]),"utf-8"))
-
             else :self.wfile.write(bytes("","utf-8"))
-
         else:
-
             self.send_response(400)
-
             self.send_header("Content-Type","text/html")
-
             self.end_headers()
-
             self.wfile.write(bytes("Invalid path","utf-8"))
-
          
-
     def do_POST(self):
-
         length_int=int(self.headers['Content-Length'])
-
         body=self.rfile.read(length_int)
-
         data=json.loads(body.decode('utf-8'))
-
         key_hash=data['user'].lower()+"%"+data['friend'].lower()
-
         # messageList[key_hash]=[*messageList[key_hash],data['message']]
-
         # sorted_keyHash = ''.join(sorted(key_hash))
-
         # if sorted_keyHash in messageList:
-
         #    messageList[sorted_keyHash].append(data['message'])
-
         # else:
-
         #    messageList[sorted_keyHash] = [data['message']]
-
         if key_hash in messageList:
-
            messageList[key_hash].append(data['message'])
-
         else:
-
            messageList[key_hash] = [data['message']]
-
         # print(messageList)
-
         self.send_response(200)
-
         self.send_header("Content-Type","application/json")
-
         self.end_headers()
-
         self.wfile.write(bytes("Message sent...I think","utf-8"))
 
-
-
 if __name__ == '__main__':
-
     server=HTTPServer((HOST,PORT),CustomConnection)
-
     print('server3 started')
-
     server.serve_forever()
